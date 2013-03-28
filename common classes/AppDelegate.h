bae94fbae48fd5967d998f6525c022794da4e148
Avinash Tag <avi.tag@gmail.com>
2013-03-28 16:45:20 +0530

    Initial Commit

---
+++ common classes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  common classes
+//
+//  Created by Vertax on 28/03/13.
+//  Copyright (c) 2013 Vertax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ common classes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  common classes
+//
+//  Created by Vertax on 28/03/13.
+//  Copyright (c) 2013 Vertax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
